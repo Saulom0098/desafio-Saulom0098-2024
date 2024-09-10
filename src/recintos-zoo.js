bbb89c64c8b7abccf3e08b0e6adf64743396312d
Saulo gabriel Moraes <130532128+Saulom0098@users.noreply.github.com>
2024-09-10 15:18:15 -0300

    feat-add contructor dos recintos e animais a serem tratador pelo zoologico

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,5 +1,22 @@
 class RecintosZoo {
+    constructor() {
+        this.recintos = [
+            { numero: 1, bioma: 'savana', tamanhoTotal: 10, animaisExistentes: [{ especie: 'MACACO', quantidade: 3 }] },
+            { numero: 2, bioma: 'floresta', tamanhoTotal: 5, animaisExistentes: [] },
+            { numero: 3, bioma: 'savana e rio', tamanhoTotal: 7, animaisExistentes: [{ especie: 'GAZELA', quantidade: 1 }] },
+            { numero: 4, bioma: 'rio', tamanhoTotal: 8, animaisExistentes: [] },
+            { numero: 5, bioma: 'savana', tamanhoTotal: 9, animaisExistentes: [{ especie: 'LEAO', quantidade: 1 }] },
+        ];
 
+        this.animais = {
+            LEAO: { tamanho: 3, biomas: ['savana'], carnivoro: true },
+            LEOPARDO: { tamanho: 2, biomas: ['savana'], carnivoro: true },
+            CROCODILO: { tamanho: 3, biomas: ['rio'], carnivoro: true },
+            MACACO: { tamanho: 1, biomas: ['savana', 'floresta'], carnivoro: false },
+            GAZELA: { tamanho: 2, biomas: ['savana'], carnivoro: false },
+            HIPOPOTAMO: { tamanho: 4, biomas: ['savana', 'rio'], carnivoro: false },
+        };
+    }
     analisaRecintos(animal, quantidade) {
     }
 
